e616796c00c0a32b665533132e852b1bdd78d62a
nadiadoudouche <76207192+nadiadoudouche@users.noreply.github.com>
2023-09-11 14:36:10 +0200

    Initial commit

---
+++ QRcode.py
@@ -0,0 +1,14 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=5
+)
+
+data = "file:///C:/Apache24/htdocs/Salle109.html"
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qr_code.png")
